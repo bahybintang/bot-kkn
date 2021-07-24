d5d8c670da23d8f185b5c8fc0341d2579d2b7c9a
Muhammad Bintang Bahy <bahybintang@gmail.com>
2021-07-24 18:10:28 +0700

    Refactor and add help command

--- index.js
+++ index.js
@@ -15,63 +15,74 @@ venom
 
 const start = (client) => {
   client.onMessage(async (message) => {
-    if (
-      (["jadwalhariini"].includes(
-        message.body.toLowerCase().replace(/\s/g, "")
-      ) ||
-        message.body.toLowerCase() == "jadwal") &&
-      message.isGroupMsg
-    ) {
-      try {
-        let dt = moment().add(HOUR_DIFF, "hours").startOf("day");
-        let schedule = await getScheduleByDate(dt);
-        const result = await client.sendText(
-          message.from,
-          getScheduleMessage(schedule, dt)
-        );
-        successLogger.info(result);
-      } catch (error) {
-        errorLogger.error(error);
-      }
-    } else if (
-      ["jadwalbesok"].includes(message.body.toLowerCase().replace(/\s/g, "")) &&
-      message.isGroupMsg
-    ) {
-      try {
-        let dt = moment().add(HOUR_DIFF, "hours").startOf("day").add(1, "days");
-        let schedule = await getScheduleByDate(dt);
-        const result = await client.sendText(
-          message.from,
-          getScheduleMessage(schedule, dt)
-        );
-        successLogger.info(result);
-      } catch (error) {
-        errorLogger.error(error);
-      }
-    } else if (
-      ["jadwal"].includes(message.body.toLowerCase().split(" ")[0]) &&
-      message.body
-        .split(" ")[1]
-        .match(
-          /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
-        ) &&
-      message.isGroupMsg
-    ) {
-      try {
-        let dt = moment(message.body.split(" ")[1], "DD-MM-YYYY").utcOffset(
-          60 * 7
-        );
-        let schedule = await getScheduleByDate(dt);
-        const result = await client.sendText(
-          message.from,
-          getScheduleMessage(schedule, dt)
-        );
-        successLogger.info(result);
-      } catch (error) {
-        errorLogger.error(error);
-      }
+    try {
+      handleMessage(client, message);
+    } catch (error) {
+      errorLogger.error(error);
     }
   });
+};
+
+const handleMessage = async (client, message) => {
+  if (
+    (["jadwalhariini"].includes(
+      message.body.toLowerCase().replace(/\s/g, "")
+    ) ||
+      message.body.toLowerCase() == "jadwal") &&
+    message.isGroupMsg
+  ) {
+    let dt = moment().add(HOUR_DIFF, "hours").startOf("day");
+    let schedule = await getScheduleByDate(dt);
+    const result = await client.sendText(
+      message.from,
+      getScheduleMessage(schedule, dt)
+    );
+    successLogger.info(result);
+  } else if (
+    ["jadwalbesok"].includes(message.body.toLowerCase().replace(/\s/g, "")) &&
+    message.isGroupMsg
+  ) {
+    let dt = moment().add(HOUR_DIFF, "hours").startOf("day").add(1, "days");
+    let schedule = await getScheduleByDate(dt);
+    const result = await client.sendText(
+      message.from,
+      getScheduleMessage(schedule, dt)
+    );
+    successLogger.info(result);
+  } else if (
+    ["jadwal"].includes(message.body.toLowerCase().split(" ")[0]) &&
+    message.body
+      .split(" ")[1]
+      .match(
+        /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
+      ) &&
+    message.isGroupMsg
+  ) {
+    let dt = moment(
+      message.body.replace("/", "-").replace(".", "-").split(" ")[1],
+      "DD-MM-YYYY"
+    );
+    let schedule = await getScheduleByDate(dt);
+    const result = await client.sendText(
+      message.from,
+      getScheduleMessage(schedule, dt)
+    );
+    successLogger.info(result);
+  } else if (message.body.toLowerCase() == "/help" && message.isGroupMsg) {
+    const result = await client.sendText(
+      message.from,
+      `*List command bot:*
+1. Menampilkan jadwal hari ini
+   command: _*Jadwal*_ atau _*Jadwal hari ini*_
+
+2. Menampilkan jadwal besok
+   command: _*Jadwal besok*_
+   
+2. Menampilkan jadwal tanggal tertentu
+   command: _*Jadwal DD/MM/YYYY*_ atau _*Jadwal DD-MM-YYYY*_`
+    );
+    successLogger.info(result);
+  }
 };
 
 const getScheduleMessage = (schedule, date) => {
